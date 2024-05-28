bb52eb8eea96ebe8678a062d60dbf8fedf08b4bd
DaniloBarcellos <danilobarcellos.pessoal@gmail.com>
2024-05-28 18:35:03 +0000

    Commit Inicial

---
+++ Calculadora.cs
@@ -0,0 +1,27 @@
+using System;
+public static class OperacoresSimples
+{
+    public static int Soma(int numero1, int numero2)
+    {
+        return numero1 + numero2;
+    }
+    
+    public static int Subtracao(int numero1, int numero2)
+    {
+        return numero1 - numero2;
+    }
+    
+    public static int Multiplicacao(int numero1, int numero2)
+    {
+        return numero1 * numero2;
+    }
+}
+
+class HelloWorld {
+  static void Main() 
+  {
+
+
+  }
+
+}
