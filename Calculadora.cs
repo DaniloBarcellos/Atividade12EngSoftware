cf775739f30466039bbddfb0bc1e6a02916c28cd
DaniloBarcellos <danilobarcellos.pessoal@gmail.com>
2024-05-28 18:40:04 +0000

    Atualizacao da divisao e da classe tratamentos

--- Calculadora.cs
+++ Calculadora.cs
@@ -15,6 +15,31 @@ public static class OperacoresSimples
     {
         return numero1 * numero2;
     }
+
+    public static float Divisao(int numero1, int numero2)
+    {
+        if (numero2 == 0)
+        {
+            return -1;
+        }
+        return numero1/numero2;
+    }
+
+    public static class Tratamentos
+{
+    public static int TratamentoGeral(string numero1)
+    {
+        int numero;
+        try
+        {
+            numero = int.Parse(numero1);
+        }catch(Exception e)
+        {
+            return 1;
+        }
+        return numero;
+    }
+}
 }
 
 class HelloWorld {
